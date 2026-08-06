f303dcbbb3f9b79d66c579b4bc6779ab72e3c622
joelcarlosfloresrojas-dotcom <joelcarlosfloresrojas@gmail.com>
2026-08-05 23:50:15 -0500

    home done

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -3,16 +3,13 @@ document.getElementById("lastModified").textContent = `Last Modified: ${document
 
 const Temperature = document.querySelector('#temperature');
 const Speed = document.querySelector('#speed');
-let Chill = document.querySelector('#chill');
+const Chill = document.querySelector('#chill');
 
-let NTemperature = Temperature.textContent;
-let NTemperature1 = parseFloat(NTemperature);
-
-let NSpeed = Speed.textContent;
-let NSpeed1 = parseFloat(NSpeed);
+const NTemperature1 = parseFloat(Temperature.textContent);
+const NSpeed1 = parseFloat(Speed.textContent);
 
 if (NTemperature1 <= 10 && NSpeed1 > 4.8) {
-    let windChill = 13.12 + (0.6215 * NTemperature1) - (11.37 * Math.pow(NSpeed1, 0.16)) + (0.3965 * NTemperature1 * Math.pow(NSpeed1, 0.16));
+    const windChill = 13.12 + (0.6215 * NTemperature1) - (11.37 * Math.pow(NSpeed1, 0.16)) + (0.3965 * NTemperature1 * Math.pow(NSpeed1, 0.16));
     Chill.textContent = windChill.toFixed(1) + " °C";
 } else {
     Chill.textContent = "N/A";
